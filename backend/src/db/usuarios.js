8ee7dd175cb9db11f28ff7fda524d14e6f4e94f0
juancruzreyl <jreyl@fi.uba.ar>
2025-11-23 17:31:14 -0300

    Agrego la api de usuarios con sus funciones del CRUD

---
+++ backend/src/db/usuarios.js
@@ -0,0 +1,52 @@
+import { Pool } from 'pg'
+ 
+const db = new Pool({
+  user: 'postgres',
+  password: 'password',
+  host: 'localhost',
+  port: 5433,
+  database: 'dreamarket_db',
+})
+
+export async function get_all_users() {
+    const response = await db.query("SELECT * FROM usuarios");
+    return response.rows;
+}
+
+export async function get_user (id_usuario) {
+    const response = await db.query("SELECT * FROM usuarios WHERE id_usuario = $1", [id_usuario]);
+    if (response.rowCount === 0){
+        return undefined;
+    }
+    return response.rows[0];
+}
+
+export async function update_user(email, nombre, apellido, telefono, direccion, id_usuario) {
+    await db.query("UPDATE usuarios SET email = $1, nombre = $2, apellido = $3, telefono = $4, direccion = $5 WHERE id_usuario = $6", [email, nombre, apellido, telefono, direccion, id_usuario])
+    return {
+        id_usuario,
+        email,
+        nombre,
+        apellido,
+        telefono,
+        direccion,
+    }
+}
+
+export async function delete_user(id_usuario) {
+    await db.query("DELETE FROM usuarios WHERE id_usuario = $1", [id_usuario]);
+}
+
+export async function create_user(email, nombre, apellido, telefono, direccion) {
+  try {
+    const result = await db.query(
+      `INSERT INTO usuarios (email, nombre, apellido, telefono, direccion)
+       VALUES ($1, $2, $3, $4, $5)
+       RETURNING id_usuario, email, nombre, apellido, telefono, direccion`,
+      [email, nombre, apellido, telefono, direccion]
+    );
+    return result.rows[0];
+  } catch {
+    return undefined;
+  }
+}
