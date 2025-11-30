4a9d13fcabee54abe188bb0f3a11493e11e5ed9d
juancruzreyl <jreyl@fi.uba.ar>
2025-11-30 16:04:51 -0300

    Agrego el cors y modifico tabla de productos

--- backend/index.js
+++ backend/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import usuarios from "./src/api_usuarios.js"
 import productos from "./src/api_productos.js"
 import pedidos from "./src/api_pedidos.js"
+import cors from "cors"
 
 const app = express()
 const port = 3000
 
 app.use(express.json());
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
